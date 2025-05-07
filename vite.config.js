dd1fd07d78c65a24c8f07d25e8ee7667db6190fa
Manmay Rawal <manmayrawal406@gmail.com>
2025-05-07 19:46:27 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Loan-calculator/',
+  base: '/Loan-calculator',
   plugins: [react()],
 });
